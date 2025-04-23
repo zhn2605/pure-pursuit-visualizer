3a4d395df761c0c2f1554bd77a386b376cd841fc
zhn2605 <zzihan0805@gmail.com>
2025-04-22 20:44:58 -0400

    cleanup and venv setup

--- src/main.py
+++ src/main.py
@@ -13,11 +13,11 @@ history = History(debug=True)
 track1 = lambda x: -((x - 5)**2) + 25           # Concave-down curve
 track2 = lambda x: math.sin(5*x)                # Basic sin wave
 track3 = lambda x: math.sin(x/5.0) * x/2.0      # Old track
-track4 = lambda x: 2 * (x < 5) - 2 * (x >= 5)   # Piecewise sharp-turn
+track4 = lambda x: 2 * (x < 5) - 2 * (x >= 5)   # Piecewise sharp-turnday, April 11th, 2025 
 
 history.generate_track(xmin=0, xmax=50, points=100, function=track3)
 
-car = Car(position=np.array([0.0, 0.0]), lookAheadDistance=12.0, velocity=1.0, max_accel=2.0)
+car = Car(position=np.array([0.0, 0.0]), lookAheadDistance=1.0, velocity=1.0, max_accel=2.0)
 pure_pursuit = PurePursuit()
 
 history.animate(car, pure_pursuit)
